a4c997d4dbf1cdc212f282c2ba9c556e49b29803
Lijun Wang <wang283808065@gmail.com>
2017-06-04 23:16:39 -0700

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,50 +1,5 @@
 #include "ftls.h"
 
-// int main(int ac, char **av)
-// {
-// 	DIR *dirp;
-// 	struct dirent *dir;
-// 	struct stat sb;
-
-
-// 	dirp = opendir("./pictures"); ///pictures
-// 	if (dirp == NULL)
-// 	{
-// 		perror("opendir");
-// 		return (0);
-// 	}
-
-// 	while ((dir = readdir(dirp)))
-// 	{
-		
-// 		if (stat(ft_strjoin("pictures/", dir->d_name), &sb) == -1)
-// 		{
-// 			perror("stat1");
-// 			exit(EXIT_FAILURE);
-// 		}
-// 		if (dir->d_name[0] != '.' && ((sb.st_mode & S_IFMT) == S_IFDIR))
-// 			printf("%s\n", dir->d_name);
-// 	}
-
-// 	exit(EXIT_SUCCESS);
-//     if (!closedir(dirp))
-//     	return (-1);
-
-// 	return (0);
-// }
-   // char *s = "ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1";
-   // printf("%d\n", (int)ft_strlen(s));
-// #define FLAG_A 1
-// #define FLAG_R 2
-// #define FLAG_CAP_R 4
-// #define FLAG_T 8
-// #define FLAG_L 16
-// #define FLAG_ONE 32
-// #define FLAG_F 64
-// #define FLAG_N 128
-// #define FLAG_U 256;
-
-//flag = flag | FLAG_A
 void  get_flag(char *av, int *flag)
 {
    while (*++av)
